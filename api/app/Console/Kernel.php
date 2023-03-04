95d517bd22dc43b147fed6e2715b3fc87f90d2e3
Ojo Oluwaseun Joseph <iamjoestack@gmail.com>
2023-03-04 10:37:31 +0100

    Get new data every hour with scheduler

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\WeatherJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(WeatherJob::class)->hourly();
     }
 
     /**
