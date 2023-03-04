0c3d7cac3ebc960aee3519c7f9ef8cd31729da89
Ojo Oluwaseun Joseph <iamjoestack@gmail.com>
2023-03-04 10:36:48 +0100

    Command to test job

---
+++ api/app/Console/Commands/WeatherCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\WeatherJob;
+use Illuminate\Console\Command;
+
+class WeatherCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:weather';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function handle(): void
+    {
+        WeatherJob::dispatch();
+    }
+}
