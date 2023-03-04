9cfc4a04657ce8f83901d62d75984e29efc76916
Ojo Oluwaseun Joseph <iamjoestack@gmail.com>
2023-03-04 11:13:52 +0100

    frontend: tailwind conf

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+  import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
+    <div
+            class="wrapper max-w-6xl mx-auto py-4 border-b border-gray-100 sm:mb-6"
+    >
+      <nav class="flex">
+        <RouterLink to="/" class="font-semibold">Home</RouterLink>
       </nav>
     </div>
   </header>
   <RouterView />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,7 +6,45 @@ import router from "./router";
 
 import "./assets/main.css";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+    faUserSecret,
+    faWind,
+    faCloudBolt,
+    faCloudMoon,
+    faCloudMoonRain,
+    faMoon,
+    faTemperatureLow,
+    faSun,
+    faEye,
+    faDroplet,
+    faSort
+} from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(
+    faUserSecret,
+    faWind,
+    faCloudBolt,
+    faCloudMoon,
+    faCloudMoonRain,
+    faMoon,
+    faTemperatureLow,
+    faSun,
+    faEye,
+    faDroplet,
+    faSort
+);
+
 const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);
